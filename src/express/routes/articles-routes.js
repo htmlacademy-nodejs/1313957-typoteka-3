9076a80233cc33bfee89cea02f5735f3aaf545c2
Евгений <foxspb2012@gmail.com>
2021-07-20 15:44:47 +0300

    Выполнено задание личного проекта: шаблонизируй это

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,9 +3,8 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/:id`, (reg, res)=>res.send(`/articles/:id`));
-articlesRouter.get(`/add`, (reg, res)=>res.send(`/articles/add`));
-articlesRouter.get(`/edit/:id`, (reg, res)=>res.send(`/articles/edit/:id`));
-articlesRouter.get(`/category/:id`, (reg, res)=>res.send(`/articles/category/:id`));
+articlesRouter.get(`/:id`, (reg, res)=>res.render(`articles/post`));
+articlesRouter.get(`/add`, (reg, res)=>res.render(`articles/new-post`));
+articlesRouter.get(`/category/:id`, (reg, res)=>res.render(`articles/articles-by-category`));
 
 module.exports = articlesRouter;
