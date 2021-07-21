252fe92b120ccb0cc5f6a31666d5ba2e3bcf8c1e
Евгений <foxspb2012@gmail.com>
2021-07-21 14:21:39 +0300

    Правки по замечаниям от наставника

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,8 +3,8 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/:id`, (reg, res)=>res.render(`articles/post`));
 articlesRouter.get(`/add`, (reg, res)=>res.render(`articles/new-post`));
+articlesRouter.get(`/:id`, (reg, res)=>res.render(`articles/post`));
 articlesRouter.get(`/edit/:id`, (reg, res)=>res.render(`articles/edit-post`));
 articlesRouter.get(`/category/:id`, (reg, res)=>res.render(`articles/articles-by-category`));
 
