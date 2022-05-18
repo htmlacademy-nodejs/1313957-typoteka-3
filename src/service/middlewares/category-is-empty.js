00b090b944e17895890371610f0455c01e33a4ed
Евгений <foxspb2012@gmail.com>
2022-05-18 17:57:27 +0300

    Выполнено задание "Рубикон".

--- src/service/middlewares/category-is-empty.js
+++ src/service/middlewares/category-is-empty.js
@@ -3,12 +3,11 @@ const {HttpCode} = require(`../../constants`);
 
 module.exports = (categoryService) => async (req, res, next) => {
   const {id} = req.params;
-  const {name} = req.body;
 
   const categoriesWithArticles = await categoryService.getArticlesByCategory(id);
 
   if (categoriesWithArticles.length !== 0) {
-    return res.status(HttpCode.BAD_REQUEST).send(`Категория "${name}" не удалена, имеет публикации`);
+    return res.status(HttpCode.BAD_REQUEST).send(`Категория "${id}" не удалена, имеет публикации`);
   }
 
   return next();
